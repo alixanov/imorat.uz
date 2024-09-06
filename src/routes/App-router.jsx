d7797c82d1dac29e05419497d291ba5de0b37509
jasurbekfrontend <jasurmominjonov2818@gmail.com>
2024-09-06 19:51:03 +0500

    routes togirladim

--- src/routes/App-router.jsx
+++ src/routes/App-router.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "../components/main/Main";
+import GetStartedPage from "../components/get-started/GetStartedPage";
+import { Login, Register } from "../page";
+import Home from "../components/home/Home";
 
 const AppRouter = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const token = localStorage.getItem("token");
+  return;
+  token ? (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  ) : (
+    <Routes>
+      <Route path="/" element={<GetStartedPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
