7956a154744d711c3c849f0410bf99e303c76a35
Алиханов <alixonovshukurullo22@gmail.com>
2024-09-06 14:45:50 +0500

    iyi

--- src/page/index.js
+++ src/page/index.js
@@ -1,3 +1,2 @@
-export { default as GetStarted } from "./get-started/Get-started-page"
 export { default as Login } from "../page/login/Login"
 export {default as Register} from "../page/register/Register"
--- src/page/login/Login.jsx
+++ src/page/login/Login.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import "../login/login.css"
-import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import "../login/login.css";
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import video from "../../assets/mixkit-aerial-view-of-a-city-during-the-night-4308-full-hd.mp4";
 
 const Login = () => {
-     const { register, handleSubmit } = useForm()
+     const { register, handleSubmit } = useForm();
+
+     // Массив данных с видео можно убрать, так как путь уже импортирован
+     // const data = [{ id: 1, video: video }];  // Эта часть не нужна
 
      return (
           <div className="login__container">
-               <form className='login__form'
-                    onSubmit={handleSubmit((data) => console.log(data))} >
+               {/* Видеофон */}
+               <video
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    className="background__video"
+               >
+                    <source src={video} type="video/mp4" />
+                    Ваш браузер не поддерживает HTML5 видео.
+               </video>
+
+               {/* Форма входа */}
+               <form className="login__form" onSubmit={handleSubmit((data) => console.log(data))}>
                     <p>Shaxsiy kabinetga o'tish</p>
-                    <input type="text" placeholder='Username' {...register("username")} />
-                    <input type="password" placeholder='Parol' {...register("password")} />
+                    <input type="text" placeholder="Username" {...register("username")} />
+                    <input type="password" placeholder="Parol" {...register("password")} />
                     <div className="page__register">
                          <Link>Ruyhatdan o'tish</Link>
-     <Link>Parol unutdingizmi ?</Link>
+                         <Link>Parol unutdingizmi?</Link>
                     </div>
                     <button type="submit">Kirish</button>
                </form>
           </div>
-     )
-}
+     );
+};
 
-export default Login
+export default Login;
